14030abb376c32f70b8b6ed7635a47dbe2dcbde8
p_alanccai <p_alanccai@tencent.com>
2023-05-24 16:54:24 +0800

    配置一级二级路由

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <el-button type="primary">i am button</el-button>
+  <!--  一级路由出口组件-->
+  <router-view />
 </template>
 <script setup>
 import { onMounted } from 'vue'
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  我是首页
+  <!--  二级路由出口组件-->
+  <router-view />
+</template>
+
+<style scoped></style>
