2f502a38366f7b8c2b32097e5b9ae614f624dfd7
p_alanccai <p_alanccai@tencent.com>
2023-05-24 16:05:34 +0800

    axios安装并简单封装

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
   <el-button type="primary">i am button</el-button>
 </template>
+<script setup>
+import { onMounted } from 'vue'
+import { getCategory } from './apis/testAPI'
+
+onMounted(() => {
+  getCategory().then((res) => {
+    console.log('testAPI', res)
+  })
+})
+</script>
