5171aeef4ec7b796d904fe7f374d82f18021b620
p_alanccai <p_alanccai@tencent.com>
2023-05-29 17:35:05 +0800

    4-05.二级分类-列表无限加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -38,6 +38,23 @@ const tabChange = () => {
   reqData.value.page = 1 //初始化页数
   getGoodList()
 }
+
+/**
+ *  无限加载实现
+ *  1. 触底条件满足之后 page++，拉取下一页数据  v-infinite-scroll
+ *  2. 新老数据做数组拼接   展开运算符
+ *  3. 判断是否已经全部加载完毕，停止监听
+ */
+const disabled = ref(false)
+const load = async () => {
+  console.log('触底了')
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value = [...goodList.value, ...res.result.items]
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+}
 </script>
 <template>
   <div class="container">
@@ -57,7 +74,7 @@ const tabChange = () => {
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
         <!-- 商品列表-->
         <GoodsItem v-for="good in goodList" :key="good.id" :goods="good"></GoodsItem>
       </div>
