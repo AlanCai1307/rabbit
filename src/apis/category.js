1dec5069db2d9107962d6524fad188e6b6aeba1a
p_alanccai <p_alanccai@tencent.com>
2023-05-29 11:27:14 +0800

    3-10.一级分类-面包屑导航渲染

---
+++ src/apis/category.js
@@ -0,0 +1,15 @@
+import request from '@/utils/http' //只有一个默认导出时可以自定义导入变量名
+
+/**
+ * @description: 获取分类数据
+ * @param id
+ * @returns {*}
+ */
+export const getTopCategoryAPI = (id) => {
+  return request({
+    url: '/category',
+    params: {
+      id
+    }
+  })
+}
